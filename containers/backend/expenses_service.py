c00b03fa75230c9e8681cd78b26797a33cd4ebf7
Sam Cowley <samuel.alexander.cowley@gmail.com>
2021-08-26 22:03:17 -0400

    Fix null check for image upload/download

--- containers/backend/expenses_service.py
+++ containers/backend/expenses_service.py
@@ -286,8 +286,7 @@ class Expenses:
             self.rds_cur.execute("SELECT image FROM {} where uid=%s and rid=%s and eid=%s".format(self.rds_expense_table), (item.uid, item.rid, item.eid))
             key = self.rds_cur.fetchall()
             self.rds_conn.commit()
-            print(key, flush=True)
-            if (key is not None or key != ""): return (405,)
+            if (len(key) == 0 or key[0][0] == None): return (405,)
 
             self.s3.meta.client.upload_file(item.file_path, self.s3_bucket, item.file_path.split('/')[2])
             os.remove(item.file_path)
@@ -304,8 +303,7 @@ class Expenses:
             self.rds_cur.execute("SELECT image FROM {} where uid=%s and image=%s".format(self.rds_expense_table), (item.uid, item.file_path))
             key = self.rds_cur.fetchall()
             self.rds_conn.commit()
-            print(key, flush=True)
-            if key is None or key == "": return (404,)
+            if (len(key) == 0 or key[0][0] == None): return (404,)
 
             file_object = self.s3.meta.client.download_file(self.s3_bucket, item.image, '/tmp/' + item.image)
             return (200, '/tmp/' + item.file_path)
